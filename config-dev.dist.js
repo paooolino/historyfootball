9c67d65a73b3ec43396b11280612dc8694a8cdb7
Paolo <paooolino@gmail.com>
2018-07-30 15:36:08 +0200

    project setup

--- config-dev.dist.js
+++
@@ -1,9 +0,0 @@
-export default {
-  ftp: {
-    HOST: 'www.example.com',
-    PORT: 22,
-    USERNAME: '',
-    PASSWORD: '',
-    REMOTEPATH: '/httpdocs'
-  }
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,7 @@
 const path = require('path');
-const WebpackFtpUpload = require('webpack-ftp-upload-plugin');
-const configDev = require('config-dev.js');
 
 module.exports = {
   entry: './src/index.js',
-  plugins: [
-    new WebpackFtpUpload({
-      host: configDev.ftp.HOST,
-      port: configDev.ftp.PORT,
-      username: configDev.ftp.USERNAME,
-      password: configDev.ftp.PASSWORD,
-      local: path.join(__dirname, 'dist'),
-      path: configDev.ftp.REMOTEPATH,
-    })
-  ]
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
