87679ce7ca13dd050580eb46339bc9615c001ce5
Paolo <paooolino@gmail.com>
2018-07-30 16:21:42 +0200

    defining state

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
-function component() {
-  var element = document.createElement('div');
+import {createStore} from 'redux';
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = 'Hello webpack';
+const regions = ['Piemonte', 'Lombardia', 'Liguria'];
+const kitTypes = ['Tinta unita', 'Quarti', 'Scacchi', 'Righe verticali', 'Righe orizzontali', 'Cerchiata', 'Scudata', 'Crociata', 'Striscia obliqua', 'V'];
+const initialState = {
+  year: 1890,
+  day: 0,
+  player: {
+    teamName: '',
+    region_id: 0,
+    teamKit: {
+      type_id: 0,
+      color1: '',
+      color2: '',
+      shortsColor: '',
+      socksColor: ''
+    },
+    balance: 500,
+    attack: 0,
+    midfield: 0,
+    defense: 0,
+    condition: 0
+  },
+  teams: ['Genoa FC', 'Internazionale Torino', 'Ginnastica Torino', 'FC Torinese']
+};
 
-  return element;
+function reducer(state, action) {
+  return state;
 }
 
-document.body.appendChild(component());
+function render() {
+}
+
+const store = createStore(reducer);
+store.subscribe(render);
