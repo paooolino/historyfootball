a272dbb764d86c4d522aec1a56d95afbb8337762
Paolo <paooolino@gmail.com>
2018-07-31 12:14:16 +0200

    team data

--- src/index.js
+++ src/index.js
@@ -1,27 +1,14 @@
 import {createStore} from 'redux';
 
+function Team() {
+}
+
 const regions = ['Piemonte', 'Lombardia', 'Liguria'];
 const kitTypes = ['Tinta unita', 'Quarti', 'Scacchi', 'Righe verticali', 'Righe orizzontali', 'Cerchiata', 'Scudata', 'Crociata', 'Striscia obliqua', 'V'];
 const initialState = {
   year: 1890,
-  day: 0,
-  player: {
-    teamName: '',
-    region_id: 0,
-    teamKit: {
-      type_id: 0,
-      color1: '',
-      color2: '',
-      shortsColor: '',
-      socksColor: ''
-    },
-    balance: 500,
-    attack: 0,
-    midfield: 0,
-    defense: 0,
-    condition: 0
-  },
-  teams: ['Genoa FC', 'Internazionale Torino', 'Ginnastica Torino', 'FC Torinese']
+  week: 1,
+  teams: []
 };
 
 function reducer(state, action) {
